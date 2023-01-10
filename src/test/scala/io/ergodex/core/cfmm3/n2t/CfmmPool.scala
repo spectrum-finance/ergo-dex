d678dc49a12ffb92469402e3ccad31a4441098d3
aleksandroranov <alexandr.oranov@gmail.com>
2023-01-10 19:31:46 +0300

    n2t v3 simulations updated.

--- src/test/scala/io/ergodex/core/cfmm3/n2t/CfmmPool.scala
+++ src/test/scala/io/ergodex/core/cfmm3/n2t/CfmmPool.scala
@@ -26,95 +26,90 @@ final case class PoolReserves(x: Long, y: Long, lp: Long)
 
 final case class PoolConfig(feeNum: Int, feeDenom: Int, emissionLP: Long, burnLP: Long, minInitialDeposit: Long)
 
-final case class CfmmPool[Ledger[_] : RuntimeState](reserves: PoolReserves, config: PoolConfig) {
+final case class CfmmPool[Ledger[_]: RuntimeState](reserves: PoolReserves, config: PoolConfig) {
 
   def updateReserves(fn: PoolReserves => PoolReserves): CfmmPool[Ledger] =
     copy(reserves = fn(reserves))
 
-  val supplyLP: Long = config.emissionLP - reserves.lp
+  val supplyLP: Long                                                     = config.emissionLP - reserves.lp
 
   import CfmmPool._
 
-  def deposit(inX: Long, inY: AssetInput[Token.Y]): Ledger[VerifiedST[(CfmmPool[Ledger],
-    AssetOutput[Token.LP], AssetOutput[Some.type])]] = RuntimeState.withRuntimeState { _ =>
-    val minByX = inX * supplyLP / reserves.x
-    val minByY = inY.value * supplyLP / reserves.y
-    val unlocked = math.min(minByX, minByY)
-    val changeX =
-      if (minByX < minByY) {
-        val diff = minByY - minByX
-        val excessY = diff * reserves.y / supplyLP
-        excessY
+  def deposit(
+    inX: Long,
+    inY: AssetInput[Token.Y]
+  ): Ledger[VerifiedST[(CfmmPool[Ledger], AssetOutput[Token.LP], AssetOutput[Some.type])]] =
+    RuntimeState.withRuntimeState { _ =>
+      val minByX   = inX * supplyLP / reserves.x
+      val minByY   = inY.value * supplyLP / reserves.y
+      val unlocked = math.min(minByX, minByY)
+      val changeX  =
+        if (minByX < minByY) {
+          val diff    = minByY - minByX
+          val excessY = diff * reserves.y / supplyLP
+          excessY
+        } else 0L
+      val changeY  = if (minByX > minByY) {
+        val diff    = minByX - minByY
+        val excessX = diff * reserves.x / supplyLP
+        excessX
       } else 0L
-    val changeY =  if (minByX > minByY) {
-      val diff = minByX - minByY
-      val excessX = diff * reserves.x / supplyLP
-      excessX
-    } else 0L
 
-    Right(
-      (
-        copy(reserves = reserves.copy(
-          reserves.x + inX - changeY,
-          reserves.y + inY.value - changeX,
-          reserves.lp - unlocked)
-        ),
-        AssetOutput(unlocked),
-        AssetOutput(math.max(changeX, changeY))
+      Right(
+        (
+          copy(reserves =
+            reserves.copy(reserves.x + inX - changeY, reserves.y + inY.value - changeX, reserves.lp - unlocked)
+          ),
+          AssetOutput(unlocked),
+          AssetOutput(math.max(changeX, changeY))
+        )
       )
-    )
-  }
+    }
 
-  def redeem(inLp: AssetInput[Token.LP]): Ledger[VerifiedST[(CfmmPool[Ledger], Long,
-    AssetOutput[Token.Y])]] = RuntimeState.withRuntimeState { _ =>
-    require(inLp.value <= supplyLP)
-    val redeemedX = inLp.value * reserves.x / supplyLP
-    val redeemedY = inLp.value * reserves.y / supplyLP
-    Right(
-      (
-        copy(reserves = reserves.copy(
-          reserves.x - redeemedX,
-          reserves.y - redeemedY,
-          reserves.lp + inLp.value)
-        ),
-        redeemedX,
-        AssetOutput(redeemedY)
+  def redeem(inLp: AssetInput[Token.LP]): Ledger[VerifiedST[(CfmmPool[Ledger], Long, AssetOutput[Token.Y])]] =
+    RuntimeState.withRuntimeState { _ =>
+      require(inLp.value <= supplyLP)
+      val redeemedX = inLp.value * reserves.x / supplyLP
+      val redeemedY = inLp.value * reserves.y / supplyLP
+      Right(
+        (
+          copy(reserves = reserves.copy(reserves.x - redeemedX, reserves.y - redeemedY, reserves.lp + inLp.value)),
+          redeemedX,
+          AssetOutput(redeemedY)
+        )
       )
-    )
-  }
+    }
 
-  def swapX(inX: Long): Ledger[VerifiedST[(CfmmPool[Ledger],
-    AssetOutput[Token.Y])]] = RuntimeState.withRuntimeState { _ =>
-    require(inX > 0)
-    val (deltaX, deltaY) =
-      (BigInt(inX), BigInt(reserves.y) * inX * config.feeNum /
-        (BigInt(reserves.x) * config.feeDenom + inX * config.feeNum))
+  def swapX(inX: Long): Ledger[VerifiedST[(CfmmPool[Ledger], AssetOutput[Token.Y])]] = RuntimeState.withRuntimeState {
+    _ =>
+      require(inX > 0)
+      val (deltaX, deltaY) =
+        (
+          BigInt(inX),
+          BigInt(reserves.y) * inX * config.feeNum /
+          (BigInt(reserves.x) * config.feeDenom + inX * config.feeNum)
+        )
 
-    Right(
-      (
-        copy(reserves = reserves.copy(
-          reserves.x + deltaX.toLong,
-          reserves.y - deltaY.toLong)
-        ),
-        AssetOutput(deltaY.toLong)
+      Right(
+        (
+          copy(reserves = reserves.copy(reserves.x + deltaX.toLong, reserves.y - deltaY.toLong)),
+          AssetOutput(deltaY.toLong)
+        )
       )
-    )
   }
 
   def swapY(inY: AssetInput[Token.Y]): Ledger[VerifiedST[(CfmmPool[Ledger], Long)]] =
     RuntimeState.withRuntimeState { _ =>
       require(inY.value > 0)
       val (deltaX, deltaY) =
-        (BigInt(reserves.x) * inY.value * config.feeNum /
-          (BigInt(reserves.y) * config.feeDenom + inY.value * config.feeNum), BigInt(inY.value))
+        (
+          BigInt(reserves.x) * inY.value * config.feeNum /
+          (BigInt(reserves.y) * config.feeDenom + inY.value * config.feeNum),
+          BigInt(inY.value)
+        )
 
       Right(
-        (copy(reserves = reserves.copy(
-          reserves.x - deltaX.toLong,
-          reserves.y + deltaY.toLong)
-        ),
-          deltaX.toLong
-        )
+        (copy(reserves = reserves.copy(reserves.x - deltaX.toLong, reserves.y + deltaY.toLong)), deltaX.toLong)
       )
     }
 }
@@ -127,23 +122,25 @@ object CfmmPool {
 
   type VerifiedST[+A] = Either[CfmmPoolErr, A]
 
-  implicit def toLedger[F[_] : RuntimeState]: ToLedger[CfmmPool[F], F] =
+  implicit def toLedger[F[_]: RuntimeState]: ToLedger[CfmmPool[F], F] =
     (pool: CfmmPool[F]) =>
       new CfmmPoolBox[F](
         boxId("cfmm_pool_box"),
         pool.reserves.x,
         DefaultCreationHeight,
-        tokens = Vector(
+        tokens         = Vector(
           tokenId("pool_NFT") -> 1L,
-          tokenId("lp") -> pool.reserves.lp,
-          tokenId("y") -> pool.reserves.y
+          tokenId("lp")       -> pool.reserves.lp,
+          tokenId("y")        -> pool.reserves.y
         ),
-        registers = Map(
-          4 -> pool.config.feeNum,
-        )
+        registers      = Map(
+          4 -> pool.config.feeNum
+        ),
+        constants      = Map.empty,
+        validatorBytes = "cfmm_pool"
       )
 
-  def init[Ledger[_] : RuntimeState](inX: Long, inY: Long, config: PoolConfig): CfmmPool[Ledger] = {
+  def init[Ledger[_]: RuntimeState](inX: Long, inY: Long, config: PoolConfig): CfmmPool[Ledger] = {
     require(inX >= config.minInitialDeposit && inY >= config.minInitialDeposit)
 
     val share = math.sqrt(inX * inY).toLong // todo: overflow
--- src/test/scala/io/ergodex/core/cfmm3/n2t/SwapSellBox.scala
+++ src/test/scala/io/ergodex/core/cfmm3/n2t/SwapSellBox.scala
@@ -1,93 +1,116 @@
 package io.ergodex.core.cfmm3.n2t
 
-import io.ergodex.core.Helpers.tokenId
+import io.ergodex.core.BoxRuntime.NonRunnable
 import io.ergodex.core.RuntimeState.withRuntimeState
 import io.ergodex.core.syntax._
-import io.ergodex.core.{Box, RuntimeState}
+import io.ergodex.core.{AnyBox, BoxSim, RuntimeState, TryFromBox}
 
-final class SwapSellBox[F[_] : RuntimeState](
-                                              override val id: Coll[Byte],
-                                              override val value: Long,
-                                              override val creationHeight: Int,
-                                              override val tokens: Vector[(Coll[Byte], Long)],
-                                              override val registers: Map[Int, Any]
-                                            ) extends Box[F] {
-  override val validatorTag = "swap_sell_order"
+final class SwapSellBox[F[_]: RuntimeState](
+  override val id: Coll[Byte],
+  override val value: Long,
+  override val creationHeight: Int,
+  override val tokens: Coll[(Coll[Byte], Long)],
+  override val registers: Map[Int, Any],
+  override val constants: Map[Int, Any],
+  override val validatorBytes: String
+) extends BoxSim[F] {
 
   override val validator: F[Boolean] =
     withRuntimeState { implicit ctx =>
+      // ===== Contract Information ===== //
+      // Name: SwapSell
+      // Description: Contract that validates user's swap from ERG to token in the CFMM n2t Pool.
+      //
+      // Constants:
+      //
+      // {1} -> RefundProp[ProveDlog]
+      // {6} -> SpectrumIsQuote[Boolean]
+      // {7} -> MaxExFee[Long]
+      // {8} -> ExFeePerTokenDenom[Long]
+      // {10} -> BaseAmount[Long]
+      // {11} -> FeeNum[Int]
+      // {13} -> PoolNFT[Coll[Byte]]
+      // {14} -> RedeemerPropBytes[Coll[Byte]]
+      // {15} -> QuoteId[Coll[Byte]]
+      // {16} -> MinQuoteAmount[Long]
+      // {20} -> ExFeePerTokenNum[Long]
+      // {24} -> SpectrumId[Coll[Byte]]
+      // {28} -> FeeDenom[Int]
+      // {29} -> MinerPropBytes[Coll[Byte]]
+      // {32} -> MaxMinerFee[Long]
+      //
+      // ErgoTree: 19e50422040008cd02217daf90deb73bdf8b6709bb42093fdfaff6573fd47b630e2d3fdd4a8193a74d0404040604020400010006020578060164059c01060204b0060203e404000e2002020202020202020202020202020202020202020202020202020202020202020e2001010101010101010101010101010101010101010101010101010101010101010e20040404040404040404040404040404040404040404040404040404040404040406020320010001010602057806011606016406010004020e20030303030303030303030303030303030303030303030303030303030303030301010404060101060203e80e691005040004000e36100204a00b08cd0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798ea02d192a39a8cc7a701730073011001020402d19683030193a38cc7b2a57300000193c2b2a57301007473027303830108cdeeac93b1a573040500050005a09c010100d801d601b2a4730000eb027301d195ed92b1a4730293b1db630872017303d807d602db63087201d603b2a5730400d604db63087203d605b27204730500d6068c720502d6079573069d9c997e720606730773087e7309067e720606d6089c730a730bedededededed938cb27202730c0001730d93c27203730e938c720501730f92720773109573117312d801d6099973139d9c720773147315959172097316d801d60ab27204731700ed938c720a017318927e8c720a020672097319909c7e8cb27202731a00020672089c9a7207731b9a9c7ec1720106731c720890b0ada5d90109639593c27209731dc17209731e731fd90109599a8c7209018c72090273207321
+      //
+      // ErgoTreeTemplate: d801d601b2a4730000eb027301d195ed92b1a4730293b1db630872017303d807d602db63087201d603b2a5730400d604db63087203d605b27204730500d6068c720502d6079573069d9c997e720606730773087e7309067e720606d6089c730a730bedededededed938cb27202730c0001730d93c27203730e938c720501730f92720773109573117312d801d6099973139d9c720773147315959172097316d801d60ab27204731700ed938c720a017318927e8c720a020672097319909c7e8cb27202731a00020672089c9a7207731b9a9c7ec1720106731c720890b0ada5d90109639593c27209731dc17209731e731fd90109599a8c7209018c72090273207321
+
       // Context (declarations here are only for simulations):
 
-      val QuoteId = SELF.R4[Coll[Byte]].get
-      val SpectrumId = tokenId("y")
-      val PoolNFT = tokenId("pool_NFT")
-      val MinerPropBytes = tokenId("miner")
-      val MaxMinerFee = 100L
-      val RedeemerPropBytes = tokenId("redeemer")
+      val RefundProp: Boolean           = getConstant(1).get
+      val SpectrumIsQuote: Boolean      = getConstant(6).get
+      val MaxExFee: Long                = getConstant(7).get
+      val ExFeePerTokenDenom: Long      = getConstant(8).get
+      val BaseAmount: Long              = getConstant(10).get
+      val FeeNum: Int                   = getConstant(11).get
+      val PoolNFT: Coll[Byte]           = getConstant(13).get
+      val RedeemerPropBytes: Coll[Byte] = getConstant(14).get
+      val QuoteId: Coll[Byte]           = getConstant(15).get
+      val MinQuoteAmount: Long          = getConstant(16).get
+      val ExFeePerTokenNum: Long        = getConstant(20).get
+      val SpectrumId: Coll[Byte]        = getConstant(24).get
+      val FeeDenom: Int                 = getConstant(28).get
+      val MinerPropBytes: Coll[Byte]    = getConstant(29).get
+      val MaxMinerFee: Long             = getConstant(32).get
 
       // Contract
-      val FeeDenom = 1000
 
-      // Those constants are replaced when instantiating order:
-      val FeeNum = 996
-      val ExFeePerTokenNum = 2L
-      val ExFeePerTokenDenom = 10L
-      val MinQuoteAmount = 800L
-      val BaseAmount = SELF.tokens(0)._2
-      val MaxExFee = 1400L
-      val SpectrumIsQuote = getVar[Boolean](0).get // todo: make sure sigma produces same templates regardless of this const.
+      val poolIn     = INPUTS(0)
 
-      val poolIn = INPUTS(0)
       // Validations
       // 1.
       val validTrade =
-      if (ctx.inputs.size >= 2 && poolIn.tokens.size == 3) { // replace with INPUTS.size
-        val poolNFT = poolIn.tokens(0)._1
+        if (INPUTS.size >= 2 && poolIn.tokens.size == 3) {
+          val poolNFT = poolIn.tokens(0)._1
 
-        val poolY = poolIn.tokens(2)
+          val poolY = poolIn.tokens(2)
 
-        val poolReservesX = poolIn.value.toBigInt
-        val poolReservesY = poolY._2.toBigInt
-        val validPoolIn = poolNFT == PoolNFT
+          val poolReservesX = poolIn.value.toBigInt
+          val poolReservesY = poolY._2.toBigInt
+          val validPoolIn   = poolNFT == PoolNFT
 
-        val rewardBox = OUTPUTS(1)
+          val rewardBox = OUTPUTS(1)
 
-        val quoteAsset = rewardBox.tokens(0)
-        val quoteAmount =
-          if (SpectrumIsQuote) {
-            val deltaQuote = quoteAsset._2.toBigInt - MaxExFee
-            deltaQuote * ExFeePerTokenDenom / (ExFeePerTokenDenom - ExFeePerTokenNum)
-          } else {
-            quoteAsset._2.toBigInt
-          }
-        // 1.1.
-        val fairExFee =
-          if (SpectrumIsQuote) true
-          else {
-            val exFee = quoteAmount * ExFeePerTokenNum / ExFeePerTokenDenom
-            val remainder = MaxExFee - exFee
-            if (remainder > 0) {
-              val spectrumRem = rewardBox.tokens(1)
-              spectrumRem._1 == SpectrumId && spectrumRem._2 >= remainder
+          val quoteAsset  = rewardBox.tokens(0)
+          val quoteAmount =
+            if (SpectrumIsQuote) {
+              val deltaQuote = quoteAsset._2.toBigInt - MaxExFee
+              deltaQuote * ExFeePerTokenDenom / (ExFeePerTokenDenom - ExFeePerTokenNum)
             } else {
-              true
+              quoteAsset._2.toBigInt
             }
-          }
+          // 1.1.
+          val fairExFee   =
+            if (SpectrumIsQuote) true
+            else {
+              val exFee     = quoteAmount * ExFeePerTokenNum / ExFeePerTokenDenom
+              val remainder = MaxExFee - exFee
+              if (remainder > 0) {
+                val spectrumRem = rewardBox.tokens(1)
+                spectrumRem._1 == SpectrumId && spectrumRem._2 >= remainder
+              } else {
+                true
+              }
+            }
 
-        val relaxedOutput = quoteAmount + 1 // handle rounding loss
-        val base_x_feeNum = BaseAmount.toBigInt * FeeNum
-        // 1.2.
-        val fairPrice = poolReservesY * base_x_feeNum <= relaxedOutput * (poolReservesX * FeeDenom + base_x_feeNum)
+          val relaxedOutput = quoteAmount + 1 // handle rounding loss
+          val base_x_feeNum = BaseAmount.toBigInt * FeeNum
+          // 1.2.
+          val fairPrice     = poolReservesY * base_x_feeNum <= relaxedOutput * (poolReservesX * FeeDenom + base_x_feeNum)
+          // 1.3.
+          val validMinerFee = OUTPUTS.map { (o: Box) =>
+            if (o.propositionBytes == MinerPropBytes) o.value else 0L
+          }.fold(0L, { (a: Long, b: Long) => a + b }) <= MaxMinerFee
 
-        val minerOut = OUTPUTS(2)
-        // 1.3.
-        val validMinerFee = (minerOut.value <= MaxMinerFee) && (minerOut.propositionBytes == MinerPropBytes)
-        // replace with
-        // val validMinerFee = OUTPUTS.map { (o: Box) =>
-        // if (o.propositionBytes == MinerPropBytes) o.value else 0L
-        // }.fold(0L, { (a: Long, b: Long) => a + b }) <= MaxMinerFee
-
-        validPoolIn &&
+          validPoolIn &&
           rewardBox.propositionBytes == RedeemerPropBytes &&
           quoteAsset._1 == QuoteId &&
           quoteAmount >= MinQuoteAmount &&
@@ -95,7 +118,15 @@ final class SwapSellBox[F[_] : RuntimeState](
           fairPrice &&
           validMinerFee
 
-      } else false
-      validTrade
+        } else false
+
+      sigmaProp(RefundProp || validTrade)
     }
+}
+
+object SwapSellBox {
+  def apply[F[_]: RuntimeState, G[_]](bx: BoxSim[G]): SwapSellBox[F]      =
+    new SwapSellBox(bx.id, bx.value, bx.creationHeight, bx.tokens, bx.registers, bx.constants, bx.validatorBytes)
+  implicit def tryFromBox[F[_]: RuntimeState]: TryFromBox[SwapSellBox, F] =
+    AnyBox.tryFromBox.translate(apply[F, NonRunnable])
 }
